66d249c1bdcf60eac2349fe3ab1b1e0a19fe4aff
Sugapriya <sugapriyams16@gmail.com>
2026-03-14 22:53:02 +0530

    Day 1 - Python setup, variables and data types

---
+++ hello1403.py
@@ -0,0 +1,21 @@
+print('hello I write my frist code')
+ 
+#Comments (Python skip comments)
+#=> Single line comments
+#=>Multiple line comments
+#=>Inline Comments
+
+#Escape Sequence
+#Escape character => \ (Back slash)
+print ("Hello \"Python\" ")
+print ('Hello \"Python\" ')
+
+#Print() Challenge
+print("Your Learning Path:  \n\t- Python Basics \n\t- Data Engineering \n\t- AI")
+
+# Its hard to write in a single 
+# So Using Triple Quotes (allows us to write text in muliple line)
+print("""Your Learning Path: 
+ \t- Python Basics 
+ \t- Data Engineering 
+ \t- AI""")
