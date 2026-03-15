4bf9ba1b0bc336ddf32348677118ff3ba8ab34c6
Sugapriya <sugapriyams16@gmail.com>
2026-03-15 15:06:34 +0530

    Day 2 - Ch 1 · Comments, print output, variables & user in...

---
+++ Day2_1503.py
@@ -0,0 +1,22 @@
+#Variable Challange
+
+name = "datawithbaraa.com"
+
+print ("info@"+name)
+print ("support@"+name)
+print ("www."+name)
+
+#input function
+input ("Enter your name")
+
+#Data Type
+a =1 #int
+b= 0.23 # float
+c= "abc" #str
+d = 'sdf' # str
+e ='123' #str
+f = True #bool
+g = False #bool
+h = None # NoneType
+i = "" #str-Blank
+j= " " #str - Empty Space
