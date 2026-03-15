ac7c9c646dd33d69d43942c3d8d8e23b33ecd1d6
Sugapriya <sugapriyams16@gmail.com>
2026-03-15 22:19:41 +0530

    "Day 3 - Ch 1 · Core data types – int, float, str, bool, Nonetype"

--- Day2_1503.py
+++ Day2_1503.py
@@ -7,7 +7,7 @@ print ("support@"+name)
 print ("www."+name)
 
 #input function
-input ("Enter your name")
+#input ("Enter your name")
 
 #Data Type
 a =1 #int
@@ -20,3 +20,16 @@ g = False #bool
 h = None # NoneType
 i = "" #str-Blank
 j= " " #str - Empty Space
+
+# Function 
+#Challange
+age = 29
+height = 5.9
+name = "sugapriya"
+student = False
+salary = None
+print ("value of age:" , age , "Data type :" , type(age), "Length :", age.bit_length)
+print ("value of age:" , height , "Data type :" , type(height), "Length :", len(str(height)))
+print ("value of age:" , name , "Data type :" , type(name),"Length :", len(name))
+print ("value of age:" , student , "Data type :" , type(student),"Length :", student.bit_length)
+print ("value of age:" , salary , "Data type :" , type(salary),"Length :", len(str(salary)))
